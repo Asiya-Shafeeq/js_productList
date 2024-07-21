48e58c376932cb79344caefe59dece1676f68e9e
Asiya-Shafeeq <asiyaass268@gmail.com>
2024-07-21 21:56:45 +0530

    js_productList

---
+++ style.js
@@ -0,0 +1,119 @@
+// fetch('https://fakestoreapi.com/products')
+//     .then(res => res.json())
+//     .then(data => {
+//         data.forEach(product => {
+//             const markup = `
+//                 <div class="category">
+//                     <span class="img">
+//                         <img src="${product.image}" alt=""/>
+//                     </span>
+//                     <div class="box">
+//                         <span class="small">
+//                             <h4>${product.title}</h4>
+//                             <h5>${product.rating ? product.rating.rate : 'No rating'}</h5>
+//                         </span>
+//                         <h5>Price: $${product.price}</h5>
+//                         <button>Buy Now</button>
+//                     </div>
+//                 </div>`;
+//             document.querySelector('.product_items').insertAdjacentHTML('beforeend', markup);
+//         });
+//     })
+//     .catch(error => console.log(error));
+
+// let selectMenu = document.querySelector('#products');
+// let heading = document.querySelector('.top h1');
+
+// selectMenu.addEventListener('change', function() {
+//     heading.innerHTML = this[this.selectedIndex].text;
+
+// })
+// function filterData(category) {
+//     if (category === 'all') {
+//         product(data);
+//     } else {
+//         const filteredData = data.filter(item => item.category === category);
+//         product(filteredData);
+//     }
+// }
+
+// // Event listener for the dropdown menu
+// document.getElementById('products').addEventListener('change', function() {
+//     const selectedCategory = this.value;
+//     filterData(selectedCategory);
+// });
+
+// // Initial display of all data
+// product(data);
+
+
+
+let data = []; // To store fetched data
+
+// Fetch product data
+fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(fetchedData => {
+        data = fetchedData;
+        displayData(data); // Initial display of all products
+    })
+    .catch(error => console.log(error));
+
+// Function to display product data
+function displayData(items) {
+    const container = document.querySelector('.product_items');
+    container.innerHTML = ''; // Clear previous data
+
+    items.forEach(product => {
+        const markup = `
+            <div class="category">
+                <span class="img">
+                    <img src="${product.image}" alt=""/>
+                </span>
+                <div class="box">
+                    <span class="small">
+                        <h4>${product.title}</h4>
+                        <h5>${product.rating ? product.rating.rate : 'No rating'}</h5>
+                    </span>
+                    <h5>Price: $${product.price}</h5>
+                    <button>Buy Now</button>
+                </div>
+            </div>`;
+        container.insertAdjacentHTML('beforeend', markup);
+    });
+}
+
+// Function to filter data based on the selected category
+function filterData(category) {
+    if (category === 'all') {
+        displayData(data);
+    } else {
+        const filteredData = data.filter(item => item.category === category);
+        displayData(filteredData);
+    }
+}
+
+// Event listener for the dropdown menu
+document.getElementById('products').addEventListener('change', function() {
+    const selectedCategory = this.value;
+    filterData(selectedCategory);
+
+    // Update heading to reflect selected category
+    document.querySelector('.top h1').textContent = this[this.selectedIndex].text;
+});
+
+    const bar = document.getElementById('bar');
+    const nav = document.getElementById('navbar');
+    const close = document.getElementById('close');
+
+    if (bar) {
+        bar.addEventListener('click', () => {
+            nav.classList.add('active');
+        })
+    }
+
+    if (close) {
+        close.addEventListener('click', () => {
+            nav.classList.remove('active');
+        })
+    } 
